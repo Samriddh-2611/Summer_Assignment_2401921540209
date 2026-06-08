1535c89e93e0c548a5dfe8b51a37a5849c81f3ce
Samriddh-2611 <samriddhtripathi2611@gmail.com>
2026-06-08 23:07:10 +0530

    Implement canConstruct method in Ransom.java

---
+++ Week_2/Day_1/Ransom.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        char[] r = ransomNote.toCharArray();
+        char[] m = magazine.toCharArray();
+        Arrays.sort(r);
+        Arrays.sort(m);
+        int i = 0, j = 0;
+        while (i < r.length && j < m.length) {
+            if (r[i] == m[j]) {
+                i++;
+                j++;
+            } else if (r[i] > m[j]) {
+                j++;
+            } else {
+                return false;
+            }
+        }
+        return i == r.length;
+    }
+}
