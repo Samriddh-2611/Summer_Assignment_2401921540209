79279cc9c28e14727d4091dbb8bc6b6e7051a9ab
Samriddh-2611 <samriddhtripathi2611@gmail.com>
2026-06-12 00:03:44 +0530

    Update DecodeString.java

--- Week_2/Day_4/DecodeString.java
+++ Week_2/Day_4/DecodeString.java
@@ -1,13 +1,31 @@
 class Solution {
-    public String reverseWords(String s) {
-        String[] words = s.split(" ");
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < words.length; i++) {
-            result.append(new StringBuilder(words[i]).reverse());
-            if (i < words.length - 1) {
-                result.append(" ");
+    public String decodeString(String s) {
+        Stack<Integer> numStack = new Stack<>();
+        Stack<String> strStack = new Stack<>();
+        String curr = "";
+        int num = 0;
+        for (char ch : s.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                num = num * 10 + (ch - '0');
+            }
+            else if (ch == '[') {
+                numStack.push(num);
+                strStack.push(curr);
+                num = 0;
+                curr = "";
+            }
+            else if (ch == ']') {
+                int times = numStack.pop();
+                String prev = strStack.pop();
+                for (int i = 0; i < times; i++) {
+                    prev += curr;
+                }
+                curr = prev;
+            }
+            else {
+                curr += ch;
             }
         }
-        return result.toString();
+        return curr;
     }
 }
